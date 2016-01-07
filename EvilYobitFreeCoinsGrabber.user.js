7c17170940b08e50b7b4e68fbd296d18e2068ee1
coins4lunch <coins4lunch@gmail.com>
2016-01-07 10:56:48 -0700

    added random timeout before clicking each button, to distribute load a little bit over time. added NOTE about working only in Chrome. bumped version

--- EvilYobitFreeCoinsGrabber.user.js
+++ EvilYobitFreeCoinsGrabber.user.js
@@ -3,7 +3,7 @@
 // @namespace http://evilcoin.xyz
 // @description Click on each coin button on Yobit's free coins page
 // @include https://yobit.net/en/freecoins/
-// @version 1
+// @version 2
 // @author coins4lunch <coins4lunch@gmail.com>
 // @grant none
 // @require http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
@@ -11,33 +11,30 @@
 
 // Clicks all "Get free coins" buttons automatically on Yobit's "free coins" page
 // Saves you time so that you don't have to click hundreds of buttons
+// NOTE: This doesn't seem to work with Firefox, so use Chrome with Tampermonkey 
 //
 // Instructions:
 // 1. Log into https://yobit.net/
 // 2. Go to page https://yobit.net/en/freecoins/
-// 3. Make sure that this Greasemonkey/Tampermonkey script is enabled
+// 3. Make sure that this Tampermonkey script is enabled
 //
 // This script will click on the "Get free coins" buttons for all eligible coins. 
-// It should take a few (or several) seconds, depending on server and Internet speeds.
+// It should take a few seconds, depending on the random timeout as well as server and Internet speeds.
 
 $(function() {   
     $('input[type=button].clGetFreeCoins').each(function() {  
         var coinName = $(this).parent().prevAll().last().text();
         var statusHtml = $(this).parent().prevAll().first().html();
 
-        var isReady = false;
         if (statusHtml && statusHtml.indexOf('ready') >= 0) {
-            isReady = true;
-        }
-
-        if (isReady) {
-            var clickEvent  = document.createEvent ('MouseEvents');
-            clickEvent.initEvent('click', true, true);
-            this.dispatchEvent(clickEvent);
-            console.log('Clicked coin', coinName);
-        }
-        else {
-            console.log('Skipped coin (not ready)', coinName);  
+            var timeout = Math.floor((Math.random() * 8000) + 1); // timeout from 0-8 seconds approximately
+            var _this = this;
+            setTimeout(function() { // click button after random timeout to diffuse load on Yobit's server
+                var clickEvent  = document.createEvent ('MouseEvents');
+                clickEvent.initEvent('click', true, true);
+                _this.dispatchEvent(clickEvent);
+                console.log('Clicked coin', coinName, 'after', timeout, 'milliseconds');
+            }, timeout);
         }
     });
 });
